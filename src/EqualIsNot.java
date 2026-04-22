5eb640745f882befbc8edb9fb3b2975c43947e95
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 15:43:18 +0400

    Add equalIsNot method and counters for Task 3

--- src/EqualIsNot.java
+++ src/EqualIsNot.java
@@ -1,4 +1,9 @@
 public class EqualIsNot {
+    public static boolean equalIsNot(String enteredText) {
+        int isCount = 0;
+        int notCount = 0;
+        return isCount == notCount;
+    }
     public static void main(String[] args){
 
     }
