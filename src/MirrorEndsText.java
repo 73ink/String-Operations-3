f9c81eecec0507719fe401060e6601463ce765c4
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 16:07:11 +0400

    Add mirror scanning loop in Task 8

--- src/MirrorEndsText.java
+++ src/MirrorEndsText.java
@@ -1,6 +1,9 @@
 public class MirrorEndsText {
     public static String mirrorEnds(String enteredText) {
         String mirrorPart = "";
+        for (int index = 0; index < enteredText.length(); index++) {
+
+        }
         return mirrorPart;
     }
     public static void main (String [] args){
