302abd9d27db67d395feb053e5156c63aff92909
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 16:02:10 +0400

    Add sameEnds method for Task 7

--- src/SameEndsText.java
+++ src/SameEndsText.java
@@ -1,4 +1,8 @@
 public class SameEndsText {
+    public static String sameEnds(String enteredText) {
+        String matchingPart = "";
+        return matchingPart;
+    }
     public static void main (String[] args){
 
     }
