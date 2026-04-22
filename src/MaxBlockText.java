14a16d4efa9aa51c9e46cd65e89b5393cdb902bf
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 16:13:18 +0400

    Add maxBlock method for Task 9

--- src/MaxBlockText.java
+++ src/MaxBlockText.java
@@ -1,4 +1,8 @@
 public class MaxBlockText {
+    public static int maxBlock(String enteredText) {
+        int largestBlock = 0;
+        return largestBlock;
+    }
     public static void main (String[] args){
 
     }
