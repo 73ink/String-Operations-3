e591392b4887c383e7af178576795546de62a2ae
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 15:31:32 +0400

    Add lowercase conversion and scan loop in Task 1

--- src/CountYZWords.java
+++ src/CountYZWords.java
@@ -1,6 +1,14 @@
+import java.util.Locale;
+
 public class CountYZWords {
     public static int countYZ(String enteredText){
         int endingCount = 0;
+        String lowerText = enteredText.toLowerCase();
+
+        for (int num = 0; num < lowerText.length(); num++){
+
+        }
+
         return endingCount;
     }
     public static void main (String[] args){
