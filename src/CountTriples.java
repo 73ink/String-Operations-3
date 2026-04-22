7c8aeb25954aa082e83899a144b08c02a4cac9e2
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 15:52:51 +0400

    Add sample tests and finalize Task 5

--- src/CountTriples.java
+++ src/CountTriples.java
@@ -13,6 +13,8 @@ public class CountTriples {
         return tripleCount;
     }
     public static void main(String[] args){
-
+        System.out.println(countTriple("abcXXXabc"));
+        System.out.println(countTriple("xxxabyyyycd"));
+        System.out.println(countTriple("a"));
     }
 }
