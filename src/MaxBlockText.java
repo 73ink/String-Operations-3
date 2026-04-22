a4c8e81fea4638aedc18dc7b2233bced9ae19953
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 16:14:46 +0400

    Add empty check and tracking values in Task 9

--- src/MaxBlockText.java
+++ src/MaxBlockText.java
@@ -1,6 +1,10 @@
 public class MaxBlockText {
     public static int maxBlock(String enteredText) {
-        int largestBlock = 0;
+        if (enteredText.length() == 0) {
+            return 0;
+        }
+        int largestBlock = 1;
+        int currentBlock = 1;
         return largestBlock;
     }
     public static void main (String[] args){
