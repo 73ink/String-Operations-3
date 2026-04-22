f2d5a25b546cefab852f03339b364af6a5ffb19e
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 16:02:51 +0400

    Add loop to compare prefix and suffix lengths in Task 7

--- src/SameEndsText.java
+++ src/SameEndsText.java
@@ -1,6 +1,11 @@
 public class SameEndsText {
     public static String sameEnds(String enteredText) {
         String matchingPart = "";
+        int maxLength = enteredText.length() / 2;
+
+        for (int length = 1; length <= maxLength; length++) {
+
+        }
         return matchingPart;
     }
     public static void main (String[] args){
