a4ef7947d899be6d2a5c30269d5ec81b9a54b550
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 15:51:17 +0400

    Add loop to scan triple positions in Task 5

--- src/CountTriples.java
+++ src/CountTriples.java
@@ -1,6 +1,9 @@
 public class CountTriples {
     public static int countTriple(String enteredText) {
         int tripleCount = 0;
+        for (int num = 0; num < enteredText.length() - 2; num++) {
+
+        }
         return tripleCount;
     }
     public static void main(String[] args){
