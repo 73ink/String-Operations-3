32da496c98895840bd6e35b027fbcf22db7c20b4
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 15:40:18 +0400

    Add lowercase values and loop structure in Task 2

--- src/WithoutText.java
+++ src/WithoutText.java
@@ -1,6 +1,14 @@
 public class WithoutText {
     public static String withoutString(String baseText, String removeText) {
         String resultText = "";
+        String lowerBaseText = baseText.toLowerCase();
+        String lowerRemoveText = removeText.toLowerCase();
+        int num = 0;
+
+        while (num < baseText.length()) {
+
+        }
+
         return resultText;
     }
     public static void main (String[] args){
