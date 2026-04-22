251c8a4dee6c66f8e273829a82ce7a27cda84527
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 15:59:49 +0400

    Implement single digit sum logic in Task 6

--- src/SumDigitsText.java
+++ src/SumDigitsText.java
@@ -2,7 +2,11 @@ public class SumDigitsText {
     public static int sumDigits(String enteredText){
         int digitTotal = 0;
         for (int num = 0; num < enteredText.length(); num++){
+            char currentCharacter = enteredText.charAt(num);
 
+            if (Character.isDigit(currentCharacter)) {
+                digitTotal += Integer.parseInt(String.valueOf(currentCharacter));
+            }
         }
         return digitTotal;
     }
