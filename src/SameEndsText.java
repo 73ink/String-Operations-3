9a15b11089a7eb6b64e1e1f6c84b9425c103511c
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 16:04:40 +0400

    Add sample tests and finalize Task 7

--- src/SameEndsText.java
+++ src/SameEndsText.java
@@ -15,6 +15,8 @@ public class SameEndsText {
         return matchingPart;
     }
     public static void main (String[] args){
-
+        System.out.println(sameEnds("abXYab"));
+        System.out.println(sameEnds("xx"));
+        System.out.println(sameEnds("xxx"));
     }
 }
