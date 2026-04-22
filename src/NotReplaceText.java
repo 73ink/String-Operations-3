cc3ea1dbdbe9b2c0cc7786c9bd9a2ccd34bbe284
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 16:24:19 +0400

    Implement standalone is replacement logic in Task 11

--- src/NotReplaceText.java
+++ src/NotReplaceText.java
@@ -2,7 +2,19 @@ public class NotReplaceText {
     public static String notReplace(String enteredText) {
         String updatedText = "";
         for (int num = 0; num < enteredText.length(); num++) {
+            if (num <= enteredText.length() - 2 && enteredText.substring(num, num + 2).equals("is")) {
+                boolean noLetterBefore = num == 0 || !Character.isLetter(enteredText.charAt(num - 1));
+                boolean noLetterAfter = num + 2 >= enteredText.length() || !Character.isLetter(enteredText.charAt(num + 2));
 
+                if (noLetterBefore && noLetterAfter) {
+                    updatedText += "is not";
+                    num++;
+                } else {
+                    updatedText += enteredText.charAt(num);
+                }
+            } else {
+                updatedText += enteredText.charAt(num);
+            }
         }
         return updatedText;
     }
