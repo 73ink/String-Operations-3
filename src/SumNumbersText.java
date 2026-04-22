e872c3a0cf720a02b6deefe6cdd20e6c47e3bd4d
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 16:19:26 +0400

    Add sumNumbers method for Task 10

--- src/SumNumbersText.java
+++ src/SumNumbersText.java
@@ -1,4 +1,9 @@
 public class SumNumbersText {
+    public static int sumNumbers(String enteredText) {
+        int totalNumbers = 0;
+        return totalNumbers;
+    }
+
     public static void main(String[] args){
 
     }
