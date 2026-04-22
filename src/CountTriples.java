255fbac3975ad6361aa29b973aaa94d0e637b8ed
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 15:52:20 +0400

    Implement triple counting logic in Task 5

--- src/CountTriples.java
+++ src/CountTriples.java
@@ -2,7 +2,13 @@ public class CountTriples {
     public static int countTriple(String enteredText) {
         int tripleCount = 0;
         for (int num = 0; num < enteredText.length() - 2; num++) {
+            char firstCharacter = enteredText.charAt(num);
+            char secondCharacter = enteredText.charAt(num + 1);
+            char thirdCharacter = enteredText.charAt(num + 2);
 
+            if (firstCharacter == secondCharacter && secondCharacter == thirdCharacter) {
+                tripleCount++;
+            }
         }
         return tripleCount;
     }
