762706745725f0b6e7486bbd612844f8b33e8d04
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 15:50:37 +0400

    Add countTriple method for Task 5

--- src/CountTriples.java
+++ src/CountTriples.java
@@ -1,4 +1,8 @@
 public class CountTriples {
+    public static int countTriple(String enteredText) {
+        int tripleCount = 0;
+        return tripleCount;
+    }
     public static void main(String[] args){
 
     }
