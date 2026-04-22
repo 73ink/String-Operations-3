dbaf8ccbdf0ae7c2b6eff3736a75a55b811db8ba
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 15:48:58 +0400

    Implement happy g checking logic in Task 4

--- src/HappyGCheck.java
+++ src/HappyGCheck.java
@@ -1,7 +1,14 @@
 public class HappyGCheck {
     public static boolean gHappy(String enteredText) {
         for (int num = 0; num < enteredText.length(); num++) {
+            if (enteredText.charAt(num) == 'g') {
+                boolean hasLeftG = num > 0 && enteredText.charAt(num - 1) == 'g';
+                boolean hasRightG = num < enteredText.length() - 1 && enteredText.charAt(num + 1) == 'g';
 
+                if (!hasLeftG && !hasRightG) {
+                    return false;
+                }
+            }
         }
         return true;
     }
