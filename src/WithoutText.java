8aab148cff2a1efd8dda5e0861ad6079b07294b7
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 15:39:32 +0400

    Add withoutString method for Task 2

--- src/WithoutText.java
+++ src/WithoutText.java
@@ -1,4 +1,8 @@
 public class WithoutText {
+    public static String withoutString(String baseText, String removeText) {
+        String resultText = "";
+        return resultText;
+    }
     public static void main (String[] args){
 
     }
