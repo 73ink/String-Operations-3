2a78e8973f56d332caad0860c2a675a51f4042de
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 15:58:58 +0400

    Add digit scanning loop in Task 6

--- src/SumDigitsText.java
+++ src/SumDigitsText.java
@@ -1,6 +1,9 @@
 public class SumDigitsText {
     public static int sumDigits(String enteredText){
         int digitTotal = 0;
+        for (int num = 0; num < enteredText.length(); num++){
+
+        }
         return digitTotal;
     }
     public static void main(String[] args){
