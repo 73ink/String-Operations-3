e89dc4e6d5823f0694bebfc4f92f2d4929fbee92
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 15:33:00 +0400

    Implement y and z word ending logic in Task 1

--- src/CountYZWords.java
+++ src/CountYZWords.java
@@ -5,10 +5,14 @@ public class CountYZWords {
         int endingCount = 0;
         String lowerText = enteredText.toLowerCase();
 
-        for (int num = 0; num < lowerText.length(); num++){
-
+        for (int num = 0; num < lowerText.length(); num++) {
+            char currentCharacter = lowerText.charAt(num);
+            if (currentCharacter == 'y' || currentCharacter == 'z') {
+                if (num == lowerText.length() - 1 || !Character.isLetter(lowerText.charAt(num + 1))) {
+                    endingCount++;
+                }
+            }
         }
-
         return endingCount;
     }
     public static void main (String[] args){
