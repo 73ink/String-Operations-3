3f36dc4be0188e7a9ad3b8787409d7c4eb6342f9
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 15:46:39 +0400

    Add sample tests and finalize Task 3

--- src/EqualIsNot.java
+++ src/EqualIsNot.java
@@ -16,6 +16,8 @@ public class EqualIsNot {
         return isCount == notCount;
     }
     public static void main(String[] args){
-
+        System.out.println(equalIsNot("This is not"));
+        System.out.println(equalIsNot("This is notnot"));
+        System.out.println(equalIsNot("noisxxnotyynotxisi"));
     }
 }
