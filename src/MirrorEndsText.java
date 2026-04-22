63eb99e0275ca7cd5aac250d938dc5a9593eff21
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 16:06:46 +0400

    Add mirrorEnds method for Task 8

--- src/MirrorEndsText.java
+++ src/MirrorEndsText.java
@@ -1,4 +1,8 @@
 public class MirrorEndsText {
+    public static String mirrorEnds(String enteredText) {
+        String mirrorPart = "";
+        return mirrorPart;
+    }
     public static void main (String [] args){
 
     }
