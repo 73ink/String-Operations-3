66b00699ca3d0086a054446745ae54f14ac1e8d1
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 15:44:13 +0400

    Add counting logic for is in Task 3

--- src/EqualIsNot.java
+++ src/EqualIsNot.java
@@ -2,6 +2,11 @@ public class EqualIsNot {
     public static boolean equalIsNot(String enteredText) {
         int isCount = 0;
         int notCount = 0;
+        for (int num = 0; num < enteredText.length() - 1; num++) {
+            if (enteredText.substring(num, num + 2).equals("is")) {
+                isCount++;
+            }
+        }
         return isCount == notCount;
     }
     public static void main(String[] args){
