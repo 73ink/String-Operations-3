612140b1287045a4f3dcab73813feb91d2c1be0b
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 15:28:40 +0400

    Add countYZ method for Task 1

--- src/CountYZWords.java
+++ src/CountYZWords.java
@@ -1,4 +1,8 @@
 public class CountYZWords {
+    public static int countYZ(String enteredText){
+        int endingCount = 0;
+        return endingCount;
+    }
     public static void main (String[] args){
 
     }
