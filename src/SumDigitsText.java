d60ffa3063dee2c844582c9d29857d739fb630b6
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 15:58:02 +0400

    Add sumDigits method for Task 6

--- src/SumDigitsText.java
+++ src/SumDigitsText.java
@@ -1,4 +1,8 @@
 public class SumDigitsText {
+    public static int sumDigits(String enteredText){
+        int digitTotal = 0;
+        return digitTotal;
+    }
     public static void main(String[] args){
 
     }
