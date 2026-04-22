8b8519c998e2a4e3e45d65663261fd7febe57824
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 16:22:58 +0400

    Add notReplace method for Task 11

--- src/NotReplaceText.java
+++ src/NotReplaceText.java
@@ -1,4 +1,8 @@
 public class NotReplaceText {
+    public static String notReplace(String enteredText) {
+        String updatedText = "";
+        return updatedText;
+    }
     public static void main(String[] args){
 
     }
