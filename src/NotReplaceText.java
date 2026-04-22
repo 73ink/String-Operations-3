8af348dd2d6dd7f37fb2375d7f6a03d034f667e9
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 16:23:38 +0400

    Add text scanning loop in Task 11

--- src/NotReplaceText.java
+++ src/NotReplaceText.java
@@ -1,6 +1,9 @@
 public class NotReplaceText {
     public static String notReplace(String enteredText) {
         String updatedText = "";
+        for (int num = 0; num < enteredText.length(); num++) {
+
+        }
         return updatedText;
     }
     public static void main(String[] args){
