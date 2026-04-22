5ddbbd3a63cee21107c2e91856d3903b7691e562
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 15:48:24 +0400

    Add scanning loop for Task 4

--- src/HappyGCheck.java
+++ src/HappyGCheck.java
@@ -1,5 +1,8 @@
 public class HappyGCheck {
     public static boolean gHappy(String enteredText) {
+        for (int num = 0; num < enteredText.length(); num++) {
+
+        }
         return true;
     }
     public static void main(String[] args) {
